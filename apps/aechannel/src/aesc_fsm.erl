eae298588a81b60d597d12ae5628adbc77265ade
Artur Puzio <cytadela8@users.noreply.github.com>
2019-01-17 21:36:45 +0100

    Add channels.get.offchain_state to websokcet API
    This allows to export channel state from node.

--- apps/aechannel/src/aesc_fsm.erl
+++ apps/aechannel/src/aesc_fsm.erl
@@ -18,6 +18,7 @@
          client_died/1,           %% (fsm())
          inband_msg/3,
          get_state/1,
+         get_offchain_state/1,
          get_poi/2]).
 
 %% Inspection and configuration functions
@@ -350,6 +351,10 @@ inband_msg(Fsm, To, Msg) ->
 -spec get_state(pid()) -> {ok, #{}}.
 get_state(Fsm) ->
     gen_statem:call(Fsm, get_state).
+
+-spec get_offchain_state(pid()) -> {ok, aesc_offchain_state:state()}.
+get_offchain_state(Fsm) ->
+    gen_statem:call(Fsm, get_offchain_state).
 
 -spec get_poi(pid(), list()) -> {ok, aec_trees:poi()} | {error, not_found}.
 get_poi(Fsm, Filter) ->
@@ -1194,6 +1199,8 @@ handle_call_(_, get_state, From, #data{ on_chain_id = ChanId
           state_hash=> StateHash,
           round     => Round},
     keep_state(D, [{reply, From, {ok, Result}}]);
+handle_call_(_, get_offchain_state, From, #data{state = State} = D) ->
+    keep_state(D, [{reply, From, {ok, State}}]);
 handle_call_(_, {get_poi, Filter}, From, #data{state = State} = D) ->
     Response =
         case aesc_offchain_state:poi(Filter, State) of
